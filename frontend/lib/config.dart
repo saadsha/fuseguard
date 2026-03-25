d7480c61408d9e33524ba73ced0c39b6ea49264a
saadsha <68491195+saadsha@users.noreply.github.com>
2026-03-26 00:25:39 +0530

    Update production API URL

--- frontend/lib/config.dart
+++ frontend/lib/config.dart
@@ -1,11 +1,11 @@
 class Config {
   static const String apiUrl = String.fromEnvironment(
     'API_URL',
-    defaultValue: 'http://192.168.1.2:5000/api',
+    defaultValue: 'https://fuseguard.onrender.com/api',
   );
 
   static const String socketUrl = String.fromEnvironment(
     'SOCKET_URL',
-    defaultValue: 'http://192.168.1.2:5000',
+    defaultValue: 'https://fuseguard.onrender.com/',
   );
 }
